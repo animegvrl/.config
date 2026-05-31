d58f06120d2f626e6cbefc923dabb68d59fd40d3
初音ミク <animegirl@noreply.codeberg.org>
2026-05-31 11:08:27 +0200

    remember cursor position nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -22,3 +22,18 @@ vim.opt.colorcolumn = "100"
 vim.cmd [[highlight ColorColumn guibg=#ff0000 ctermbg=NONE]]
 
 vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true })
+
+-- remember last position of cursor when opening a file
+vim.api.nvim_create_augroup("last_pos", { clear = true })
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = "last_pos",
+  pattern = "*",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')  -- returns {lnum, col}
+    local lnum = mark[1]
+    local col = mark[2]
+    if lnum > 0 and lnum <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, {lnum, col})
+    end
+  end,
+})
